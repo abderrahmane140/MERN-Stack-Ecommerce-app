9ff20649344e2a2d76b349ee6dae984e07c936c4
bsar <bsarabdu@email.com>
2024-04-08 01:11:28 +0000

    complete profile page UI

--- frontend/src/pages/UsreProfil.jsx
+++ frontend/src/pages/UsreProfil.jsx
@@ -23,23 +23,22 @@ export default function UsreProfil() {
     }
     return (
         <>
-            <div className="flex justify-center">
-                <img src={user.avtare}  alt="hh" />
-            </div>
-            <div className="user">
-                <form onSubmit={e=>e.preventDefault()}>
-                    <input type="text" placeholder="email" value={user.email} onChange={e=>setEmail(e.target.value)} />
+            <div className="p-3 max-w-lg mx-auto">
+                <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+                <form onSubmit={e=>e.preventDefault()} className="flex flex-col gap-1">
+                <img src={user.avtare} className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" alt="profile" />
+                    <input type="text" placeholder="email" className="border p-2 rounded-lg" id="email" onChange={e=>setEmail(e.target.value)} />
 
-                    <input type="text" placeholder="username" value={user.username} onChange={e=>setUsername(e.target.value)} />
+                    <input type="text" placeholder="username"  className="border p-2 rounded-lg" id="username"  onChange={e=>setUsername(e.target.value)} />
 
-                    <input type="text" placeholder="password" onChange={e=>setPassword(e.target.value)}/>
+                    <input type="text" placeholder="password" className="border p-2 rounded-lg" id="password" onChange={e=>setPassword(e.target.value)}/>
 
-                    <button onClick={handleClick} disabled={isLoding}>{isLoding?"LOADING...":"UPDATE"}</button>
+                    <button className="bg-slate-700 text-white rounded-lg p-2 upercase hover:opacity-95 disabled:opacity-80" onClick={handleClick} disabled={isLoding}>{isLoding?"LOADING...":"UPDATE"}</button>
 
-                    <div className="flex justify-between">
-                        <span className="mt-4 cursor-pointer" onClick={handleLogout}>logout</span>
+                    <div className="flex justify-between mt-3">
+                        <span className="text-red-700 font-medium cursor-pointer" onClick={handleLogout}>Sign out</span>
                         
-                        <span className="mt-4 cursor-pointer" onClick={handleDlete}>Delete</span>
+                        <span className="text-red-700 font-medium cursor-pointer" onClick={handleDlete}>Delete</span>
                     </div>
 
                     {error && <div className="error">{error}</div>}
